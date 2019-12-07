7ebbcae12c4e7c4a5c7fcc50ebb0584a7f0119be
mfteloglu <43525350+mfteloglu@users.noreply.github.com>
2019-12-07 13:44:50 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,20 @@
-from datetime import datetime
+from flask import Flask
 
-from flask import Flask, render_template
+import views
 
+    
+def create_app():
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/comments", view_func=views.comments_page)
+    app.add_url_rule("/earthquakes", view_func=views.earthquakes_page)
+    app.config["DEBUG"] = True
+    
 
-
-@app.route("/")
-def home_page():
-    today = datetime.today()
-    day_name = today.strftime("%A")
-    return render_template("home.html", aaa = "to be connected",day=day_name)
-
-@app.route("/movies")
-def movies_page():
-    return "many comments will be added"
+    return app
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app = create_app()
+    app.run()

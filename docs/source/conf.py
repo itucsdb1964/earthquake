ea29401ac33f977e2ffec844386bdcd7271b1e4d
zeynepgurler <32238336+zeynepgurler@users.noreply.github.com>
2019-12-11 18:10:51 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ITUCSDB18NN'
-copyright = '2018, ITUCSDB18NN'
-author = 'ITUCSDB18NN'
+project = 'ITUCSDB1964'
+copyright = '2019, ITUCSDB1964'
+author = 'ITUCSDB1964'
 
 # The short X.Y version
 version = ''

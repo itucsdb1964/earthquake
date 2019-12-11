c4808430a050cf6483e1fa0669f517761616395f
mfteloglu <43525350+mfteloglu@users.noreply.github.com>
2019-12-11 18:50:00 +0300

    Update server.py

--- server.py
+++ server.py
@@ -7,8 +7,8 @@ def create_app():
     app = Flask(__name__)
 
     app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/comments", view_func=views.comments_page)
-    app.add_url_rule("/earthquakes", view_func=views.earthquakes_page)
+    app.add_url_rule("/comments", view_func=views.comments_page,methods=["GET", "POST"])
+    app.add_url_rule("/earthquakes", view_func=views.earthquakes_page,methods=["GET", "POST"])
     app.config["DEBUG"] = True
     
 

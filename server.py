b252cdbf8cb251e8b947329be3b6d14a31bf9347
mfteloglu <43525350+mfteloglu@users.noreply.github.com>
2019-12-11 18:24:45 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 from flask import Flask
 
+import views
 
-app = Flask(__name__)
+    
+def create_app():
+    app = Flask(__name__)
 
+    app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/comments", view_func=views.comments_page)
+    app.add_url_rule("/earthquakes", view_func=views.earthquakes_page)
+    app.config["DEBUG"] = True
+    
 
-@app.route("/")
-def home_page():
-    return "My movie collection"
+    return app
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
 
 if __name__ == "__main__":
     app = create_app()

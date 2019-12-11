027745194f9e29ec8158744f92c45ea8cfb34cba
zeynepgurler <32238336+zeynepgurler@users.noreply.github.com>
2019-12-11 16:10:04 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,20 +1,13 @@
 from flask import Flask
 
-import views
 
-    
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/comments", view_func=views.comments_page)
-    app.add_url_rule("/earthquakes", view_func=views.earthquakes_page)
-    app.config["DEBUG"] = True
-    
 
-    return app
+@app.route("/")
+def home_page():
+    return "Hello, world!"
 
 
 if __name__ == "__main__":
-    app = create_app()
     app.run()

549abc8a19b66bf52edee200232856ec58768bdb
mfteloglu <43525350+mfteloglu@users.noreply.github.com>
2019-12-14 00:22:15 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,9 +1,35 @@
 import pyodbc
 from datetime import datetime
 
-from flask import render_template
+from flask import current_app, render_template,request,redirect,url_for
 
+def add_earthquake_page():
+    if request.method == "GET":
+        return render_template("add_earthquake.html")
+    else:
+        form_agency = request.form["agency"]
+        form_date_time = request.form["date_time"]
+        form_latitude = request.form["latitude"]
+        form_longitude = request.form["longitude"]
 
+        print(form_agency)
+        print(form_date_time)
+        print(form_latitude)
+        print(form_longitude)
+        return redirect(url_for("add_earthquake_page"))
+
+def add_comment_page():
+    if request.method =="GET":
+        return render_template("add_comment.html")
+    else:
+        form_id = request.form["id"]
+        form_topic = request.form["topic"]
+        form_comment = request.form["comment"]
+
+        print(form_id)
+        print(form_topic)
+        print(form_comment)
+        return redirect(url_for("add_comment_page"))
 
 def earthquakes_page():
     return "bbb"

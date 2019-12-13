ab5aa14d22188cd67e284bf78617a39e5bf2df7c
mfteloglu <43525350+mfteloglu@users.noreply.github.com>
2019-12-14 00:21:43 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,26 @@
+''' from datetime import datetime
+
+from flask import Flask, render_template
+from connection import all_database
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    today = datetime.today()
+    day_name = today.strftime("%A")
+    return render_template("home.html", aaa ="bbb" ,day=day_name)
+
+@app.route("/movies")
+def movies_page():
+    return "aaaa"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080,debug=True)   '''
+
+
 from flask import Flask
 
 import views
@@ -9,6 +32,8 @@ def create_app():
     app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/comments", view_func=views.comments_page)
     app.add_url_rule("/earthquakes", view_func=views.earthquakes_page)
+    app.add_url_rule("/new_earthquake", view_func=views.add_earthquake_page, methods=["GET", "POST"])
+    app.add_url_rule("/new_comment", view_func=views.add_comment_page, methods=["GET", "POST"])
     app.config["DEBUG"] = True
     
 

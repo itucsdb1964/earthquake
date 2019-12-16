a0433d0fad1f87ffca7b245502818c724755c1eb
zeynepgurler <32238336+zeynepgurler@users.noreply.github.com>
2019-12-16 17:55:25 +0300

    Update server.py

--- server.py
+++ server.py
@@ -27,7 +27,7 @@ def create_app():
     app.add_url_rule("/logout", view_func=views.logout_page)
     app.add_url_rule("/makecomment", view_func=views.make_comment_page,methods=["GET", "POST"])
     app.add_url_rule("/comments", view_func=views.comments_page, methods=["GET","POST"])
-    app.add_url_rule("/earthquakes", view_func=views.earthquakes_page)
+    app.add_url_rule("/earthquakes", view_func=views.earthquakes_page, method = ["GET","POST"])
     app.add_url_rule("/new_earthquake", view_func=views.add_earthquake_page, methods=["GET", "POST"])
     app.add_url_rule("/signout", view_func=views.signout_page)
     app.add_url_rule("/makeessay", view_func=views.make_essay_page,methods=["GET", "POST"])

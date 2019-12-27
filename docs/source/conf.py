90645cb3bd91efdb35dcc07bac4ebc53a701aab8
zeynepgurler <32238336+zeynepgurler@users.noreply.github.com>
2019-12-27 20:33:53 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.githubpages',
+    'sphinx.ext.githubpages','rst2pdf.pdfbuilder',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -174,7 +174,6 @@ epub_title = project
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
 
-
+pdf_documents = [('index', u'documentation', u'ITUCSDB1964', u'Zeynep GÜRLER and Fatih TELOĞLU'),]
 # -- Extension configuration -------------------------------------------------
-
 
